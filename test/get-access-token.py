ddc94f79df3380cf9a5b6422b8c3a722afe521bc
Simon Eisenmann <s.eisenmann@kopano.com>
2019-04-12 17:09:03 +0200

    Improve access token python example

--- test/get-access-token.py
+++ test/get-access-token.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 """
-Simple example how to utilize OAuth2 with Kopano.
+Simple example how to get OAuth2 credentials from the commandline.
+
+Usage: ISS=https://your-kopano.local SCOPE="openid kopano/gc" \
+         CLIENT_ID=your-client-id CLIENT_SECRET=your-client-secret \
+         ./get-access-token.py --output .auth --format json
+
+  See `./get-access-token.py --help for further usage infos.
+
+Environment variables supported:
+
+  ISS           : Issuer identifier (required).
+  CLIENT_ID     : Client ID to use for OAuth2 requests.
+  CLIENT_SECRET : Client secret to use for OAuth token request.
+  SCOPE         : Scope to use for OAuth2 authorize request.
+  PROMPT        : Prompt value to use for OAuth2 authorize request.
+
+  INSECURE=1    : Disables TLS validation.
+
 """
 
 import codecs
@@ -11,7 +28,6 @@ import os
 import urllib.parse
 import queue
 import threading
-import webbrowser
 import sys
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -24,7 +40,7 @@ HTTPD_HOST = os.environ.get("HTTPD_HOST", "localhost")
 HTTPD_PORT = int(os.environ.get("HTTPD_PORT", "8080"), 10)
 
 
-class Client:
+class RedirectAuthenticator(object):
     insecure = INSECURE
     httpd_host = HTTPD_HOST
     httpd_port = HTTPD_PORT
@@ -38,11 +54,12 @@ class Client:
         self.client_secret = client_secret
 
         self.q = queue.Queue()
+        self.httpd = None
 
-        self.httpd = HTTPServer((self.httpd_host, self.httpd_port), Handler)
-        self.httpd.q = self.q
+    def start(self, noauth_local_webserver=False):
+        if self.httpd is not None and not noauth_local_webserver:
+            raise RuntimeError("already started")
 
-    def start(self):
         oidc = json.loads(requests.get(
             iss+"/.well-known/openid-configuration",
             verify=not self.insecure,
@@ -62,20 +79,47 @@ class Client:
             prompt=self.prompt,
         )
 
-        serve = threading.Thread(target=self.serve)
-        serve.start()
+        if not noauth_local_webserver:
+            import webbrowser
+            webbrowser.open(authorization_url)
+            print('Your browser has been opened to visit:')
+            print()
+            print('    ' + authorization_url)
+            print('If your browser is on a different machine then '
+                  'exit and re-run this')
+            print('application with the command-line parameter ')
+            print()
+            print('  --noauth_local_webserver')
+            print()
+            httpd = HTTPServer(
+                (self.httpd_host, self.httpd_port),
+                handlerFactory(self),
+            )
+            self.httpd = httpd
+            serve = threading.Thread(target=lambda: httpd.serve_forever())
+            serve.start()
 
-        print(authorization_url)
-        webbrowser.open(authorization_url)
+            authorization_response = self.q.get()
 
-        authorization_response = self.q.get()
+            if self.httpd is httpd:
+                httpd.shutdown()
+                self.httpd = None
+            serve.join()
 
-        if self.httpd is not None:
-            self.httpd.shutdown()
-        serve.join()
+        else:
+            print('Go to the following link in your browser:')
+            print()
+            print('    ' + authorization_url)
+            print()
+
+            authorization_response = input("Enter full callback URL: ").strip()
+            if not authorization_response:
+                raise ValueError("no callback URL given")
 
         if authorization_response is None:
             return
+
+        print("Authentication successful.")
 
         token = oauth.fetch_token(
             oidc['token_endpoint'],
@@ -87,32 +131,65 @@ class Client:
 
         return token
 
-    def serve(self):
-        self.httpd.serve_forever()
-
     def shutdown(self):
         if self.httpd is not None:
             self.httpd.shutdown()
             self.httpd = None
-        self.q.put(None)
+            self.q.put(None)
 
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+def handlerFactory(app):
+    class Handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
 
-        self.send_header('Content-type','text/html')
-        self.end_headers()
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-        message = "Done - you can close this window now."
-        self.wfile.write(bytes(message, "UTF-8"))
+            self.wfile.write(okPageHTML)
 
-        self.server.q.put(self.path)
+            app.q.put(self.path)
 
-    def log_request(self, *args, **kwargs):
-        pass
+        def log_request(self, *args, **kwargs):
+            pass
+
+    return Handler
+
+okPageHTML = b"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>OAuth2 callback</title>
+<style>
+body {
+    font-family: sans-serif;
+}
+</style>
+</head>
+<body>
+Done - you can close this window now.
+</body>
+</html>
+"""
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='OAuth2 with Kopano.')
+    parser.add_argument('--auth_host_name', default=HTTPD_HOST,
+                        help='Hostname when running a local web server.')
+    parser.add_argument('--auth_host_port', default=[HTTPD_PORT], type=int,
+                        nargs='*', help='Port web server should listen on.')
+    parser.add_argument('--noauth_local_webserver', action='store_true',
+                        default=False, help='Do not run a local web server.')
+    parser.add_argument('--output', help='Write auth result to file.')
+    parser.add_argument('--force', action='store_true',
+                        default=False, help='Allow overwrite of output file.')
+    parser.add_argument('--format', default="env", choices=["env", "json"],
+                        help='Selects the output format.')
+
+    flags = parser.parse_args()
+    print(flags)
+
     iss = os.environ.get("ISS", None)
     if not iss:
         print("No ISS in environment - abort", file=sys.stderr)
@@ -134,18 +211,32 @@ if __name__ == "__main__":
     print("CLIENT_ID           : {}".format(client_id))
     client_secret = os.environ.get("CLIENT_SECRET")
     try:
-        client = Client(iss, scope, client_id, client_secret)
+        app = RedirectAuthenticator(iss, scope, client_id, client_secret)
         if prompt is not None:
-            client.prompt = prompt
-        token = client.start()
-        at = token['access_token']
-        print("ACCESS_TOKEN_VALUE  : {}".format(at))
-        rt = token.get('refresh_token')
-        if rt:
-            print("REFRESH_TOKEN_VALUE : {}".format(rt))
+            app.prompt = prompt
+        token = app.start(noauth_local_webserver=flags.noauth_local_webserver)
+        output = sys.stdout
+        try:
+            if flags.output:
+                output = open(flags.output, flags.force and "w" or "x")
+            if flags.format == "json":
+                print(json.dumps(token, indent=2, sort_keys=True), file=output)
+            elif flags.format == "env":
+                print("ACCESS_TOKEN_VALUE={}".format(token["access_token"]), file=output)
+                print("EXPIRES_AT={}".format(token["expires_at"]), file=output)
+                print("EXPIRES_IN={}".format(token["expires_in"]), file=output)
+                print("TOKEN_TYPE={}".format(token["token_type"]), file=output)
+                rt = token.get("refresh_token")
+                if rt:
+                    print("REFRESH_TOKEN_VALUE : {}".format(rt), file=output)
+            else:
+                raise ValueError("unknown output format", flags.format)
+        finally:
+            if flags.output:
+                output.close()
 
     except KeyboardInterrupt:
         print("Interrupted", file=sys.stderr)
-        client.shutdown()
+        app.shutdown()
         sys.exit(1)
         pass
